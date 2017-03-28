702bb4c0272a2752eddb51d94741e0de073a0ba4
lius55 <tony56recruit@gmail.com>
2017-03-28 17:20:12 +0900

    modify images

--- diag/js/rule.js
+++ diag/js/rule.js
@@ -5,7 +5,7 @@ String.prototype.replaceAll = function(search, replacement) {
 
 window.onload = function() {
 
-	// 質問リスト
+	// 質問リスト(titleImage: 質問画像、fstAnswer: 一つ目の回答、sndAnswer: 二つ目の回答)
 	var questionList = [
 		{ titleImage: 'qst_01.png', fstAnswer: 'ans_03.png', sndAnswer: 'ans_04.png' },
 		{ titleImage: 'qst_02.png', fstAnswer: 'ans_05.png', sndAnswer: 'ans_06.png' },
@@ -19,86 +19,114 @@ window.onload = function() {
 		{ titleImage: 'qst_10.png', fstAnswer: 'ans_12.png', sndAnswer: 'ans_13.png' }
 	];
 
-	// 商品表示パターン
-	var goodsPattern = {
-		a:   'goods_05.png,goods_06.png,goods_07.png,goods_08.png',
-		aa:  'goods_04.png,goods_05.png,goods_07.png,goods_08.png',
-		b:   'goods_05.png,goods_06.png,goods_07.png',
-		bb:  'goods_04.png,goods_05.png,goods_07.png',
-		sb:  'goods_06.png,goods_07.png,goods_08.png',
-		sbb: 'goods_04.png,goods_07.png,goods_08.png',
-		c:   'goods_06.png,goods_07.png',
-		cc:  'goods_04.png,goods_07.png',
-		wa:  'goods_01.png,goods_02.png,goods_03.png,goods_05.png',
-		wb:  'goods_02.png,goods_03.png,goods_05.png',
-		swb: 'goods_01.png,goods_02.png,goods_03.png',
-		wc:  'goods_02.png,goods_03.png'
-	}
-
-	// 結果判定ルール
-	var rules = {
-		"AAA-A-A-AA": { num: '01', result: 'result_02.png', status: 'status_01.png', advice: 'adv_01.png', goods: goodsPattern.a   },
-		"AAA-A-A-BA": { num: '02', result: 'result_02.png', status: 'status_01.png', advice: 'adv_01.png', goods: goodsPattern.a   },
-		"AAA-A-B--A": { num: '03', result: 'result_03.png', status: 'status_02.png', advice: 'adv_02.png', goods: goodsPattern.b   },
-		"AAB-A-A-AA": { num: '04', result: 'result_04.png', status: 'status_03.png', advice: 'adv_03.png', goods: goodsPattern.aa  },
-		"AAB-A-A-BA": { num: '05', result: 'result_04.png', status: 'status_03.png', advice: 'adv_03.png', goods: goodsPattern.aa  },
-		"AAB-A-B--A": { num: '06', result: 'result_05.png', status: 'status_04.png', advice: 'adv_04.png', goods: goodsPattern.sb  },
-		"AAB-B-A-AA": { num: '07', result: 'result_04.png', status: 'status_03.png', advice: 'adv_03.png', goods: goodsPattern.aa  },
-		"AAB-B-A-BA": { num: '08', result: 'result_04.png', status: 'status_03.png', advice: 'adv_03.png', goods: goodsPattern.aa  },
-		"AAB-B-B--A": { num: '09', result: 'result_05.png', status: 'status_02.png', advice: 'adv_04.png', goods: goodsPattern.bb  },
-		"ABA---A-AA": { num: '10', result: 'result_02.png', status: 'status_01.png', advice: 'adv_01.png', goods: goodsPattern.a   },
-		"ABA---A-BA": { num: '11', result: 'result_02.png', status: 'status_01.png', advice: 'adv_01.png', goods: goodsPattern.a   },
-		"ABA---B--A": { num: '12', result: 'result_03.png', status: 'status_04.png', advice: 'adv_02.png', goods: goodsPattern.b   },
-		"ABB---A-AA": { num: '13', result: 'result_02.png', status: 'status_01.png', advice: 'adv_01.png', goods: goodsPattern.a   },
-		"ABB---A-BA": { num: '14', result: 'result_02.png', status: 'status_01.png', advice: 'adv_01.png', goods: goodsPattern.a   },
-		"ABB---B--A": { num: '15', result: 'result_03.png', status: 'status_04.png', advice: 'adv_02.png', goods: goodsPattern.b   },
-		"BAA---A--A": { num: '16', result: 'result_01.png', status: 'status_05.png', advice: 'adv_05.png', goods: goodsPattern.wa  },
-		"BAA---B--A": { num: '17', result: 'result_06.png', status: 'status_05.png', advice: 'adv_05.png', goods: goodsPattern.wb  },
-		"BAB---A--A": { num: '18', result: 'result_01.png', status: 'status_05.png', advice: 'adv_05.png', goods: goodsPattern.wa  },
-		"BAB---B--A": { num: '19', result: 'result_06.png', status: 'status_05.png', advice: 'adv_05.png', goods: goodsPattern.wb  },
-		"BBA---A--A": { num: '20', result: 'result_01.png', status: 'status_05.png', advice: 'adv_05.png', goods: goodsPattern.wa  },
-		"BBA---B--A": { num: '21', result: 'result_06.png', status: 'status_05.png', advice: 'adv_05.png', goods: goodsPattern.wb  },
-		"BBB---A--A": { num: '22', result: 'result_01.png', status: 'status_05.png', advice: 'adv_05.png', goods: goodsPattern.wa  },
-		"BBB---B--A": { num: '23', result: 'result_06.png', status: 'status_05.png', advice: 'adv_05.png', goods: goodsPattern.wb  },
-		"AAA-A-A-AB": { num: '24', result: 'result_07.png', status: 'status_04.png', advice: 'adv_06.png', goods: goodsPattern.sb  },
-		"AAA-A-A-BB": { num: '25', result: 'result_07.png', status: 'status_04.png', advice: 'adv_06.png', goods: goodsPattern.sb  },
-		"AAA-A-B--B": { num: '26', result: 'result_08.png', status: 'status_06.png', advice: 'adv_07.png', goods: goodsPattern.c   },
-		"AAB-A-A-AB": { num: '27', result: 'result_07.png', status: 'status_04.png', advice: 'adv_08.png', goods: goodsPattern.sbb },
-		"AAB-A-A-BB": { num: '28', result: 'result_07.png', status: 'status_04.png', advice: 'adv_08.png', goods: goodsPattern.sbb },
-		"AAB-A-B--B": { num: '29', result: 'result_09.png', status: 'status_06.png', advice: 'adv_09.png', goods: goodsPattern.cc  },
-		"AAB-B-A-AB": { num: '30', result: 'result_07.png', status: 'status_04.png', advice: 'adv_08.png', goods: goodsPattern.sbb },
-		"AAB-B-A-BB": { num: '31', result: 'result_07.png', status: 'status_04.png', advice: 'adv_08.png', goods: goodsPattern.sbb },
-		"AAB-B-B--B": { num: '32', result: 'result_09.png', status: 'status_06.png', advice: 'adv_09.png', goods: goodsPattern.cc  },
-		"ABA---A-AB": { num: '33', result: 'result_07.png', status: 'status_04.png', advice: 'adv_06.png', goods: goodsPattern.sb  },
-		"ABA---A-BB": { num: '34', result: 'result_07.png', status: 'status_04.png', advice: 'adv_06.png', goods: goodsPattern.sb  },
-		"ABA---B--B": { num: '35', result: 'result_08.png', status: 'status_06.png', advice: 'adv_07.png', goods: goodsPattern.c   },
-		"ABB---A-AB": { num: '36', result: 'result_07.png', status: 'status_04.png', advice: 'adv_06.png', goods: goodsPattern.sb  },
-		"ABB---A-BB": { num: '37', result: 'result_07.png', status: 'status_04.png', advice: 'adv_06.png', goods: goodsPattern.sb  },
-		"ABB---B--B": { num: '38', result: 'result_08.png', status: 'status_06.png', advice: 'adv_07.png', goods: goodsPattern.c   },
-		"BAA---A--B": { num: '39', result: 'result_10.png', status: 'status_07.png', advice: 'adv_10.png', goods: goodsPattern.swb },
-		"BAA---B--B": { num: '40', result: 'result_11.png', status: 'status_08.png', advice: 'adv_11.png', goods: goodsPattern.wc  },
-		"BAB---A--B": { num: '41', result: 'result_10.png', status: 'status_07.png', advice: 'adv_10.png', goods: goodsPattern.swb },
-		"BAB---B--B": { num: '42', result: 'result_11.png', status: 'status_08.png', advice: 'adv_11.png', goods: goodsPattern.wc  },
-		"BBA---A--B": { num: '43', result: 'result_10.png', status: 'status_07.png', advice: 'adv_10.png', goods: goodsPattern.swb },
-		"BBA---B--B": { num: '44', result: 'result_11.png', status: 'status_08.png', advice: 'adv_11.png', goods: goodsPattern.wc  },
-		"BBB---A--B": { num: '45', result: 'result_10.png', status: 'status_07.png', advice: 'adv_10.png', goods: goodsPattern.swb },
-		"BBB---B--B": { num: '46', result: 'result_11.png', status: 'status_08.png', advice: 'adv_11.png', goods: goodsPattern.wc  }
+	// 商品リスト(fileName: 商品画像名、url: リンク先)
+	var goodsList = {
+		goods01: { fileName: 'goods_01.png', url: 'http://item.rakuten.co.jp/cerapure/kl-01/#cart' },
+		goods02: { fileName: 'goods_02.png', url: 'http://item.rakuten.co.jp/cerapure/ka-02/#cart' },
+		goods03: { fileName: 'goods_03.png', url: 'http://item.rakuten.co.jp/cerapure/ka-01/#cart' },
+		goods04: { fileName: 'goods_04.png', url: 'http://item.rakuten.co.jp/cerapure/kf-03/#cart' },
+		goods05: { fileName: 'goods_05.png', url: 'http://item.rakuten.co.jp/cerapure/kf-01/#cart' },
+		goods06: { fileName: 'goods_06.png', url: 'http://item.rakuten.co.jp/cerapure/kf-04/#cart' },
+		goods07: { fileName: 'goods_07.png', url: 'http://item.rakuten.co.jp/cerapure/kf-06/#cart' },
+		goods08: { fileName: 'goods_08.png', url: 'http://item.rakuten.co.jp/cerapure/kf-02/#cart' }
 	};
 
-	// 診断質問DOM作成
-	$.each(questionList, function(index, val){
-		var num = parseInt(index + 1);
-		$("#question-section").loadTemplate(
-			$("#question-template"), {
-				questionId: "question" + num,
-				questionNum: "Q" + num + "/Q" + questionList.length,
-				titleImage: "images/" + val.titleImage,
-				fstAnswer: "images/" + val.fstAnswer,
-				sndAnswer: "images/" + val.sndAnswer
-		},{
-			append: true
-		});
-	});
+	// 商品表示パターン(表示順番)
+	var goodsPattern = {
+		a:   [ goodsList.goods05, goodsList.goods06, goodsList.goods07, goodsList.goods08 ],
+		aa:  [ goodsList.goods04, goodsList.goods05, goodsList.goods07, goodsList.goods08 ],
+		b:   [ goodsList.goods05, goodsList.goods06, goodsList.goods07 ],
+		bb:  [ goodsList.goods04, goodsList.goods05, goodsList.goods07 ],
+		sb:  [ goodsList.goods06, goodsList.goods07, goodsList.goods08 ],
+		sbb: [ goodsList.goods04, goodsList.goods07, goodsList.goods08 ],
+		c:   [ goodsList.goods06, goodsList.goods07 ],
+		cc:  [ goodsList.goods04, goodsList.goods07 ],
+		wa:  [ goodsList.goods01, goodsList.goods02, goodsList.goods03, goodsList.goods05 ],
+		wb:  [ goodsList.goods02, goodsList.goods03, goodsList.goods05 ],
+		swb: [ goodsList.goods01, goodsList.goods02, goodsList.goods03 ],
+		wc:  [ goodsList.goods02, goodsList.goods03 ]
+	};
+
+	// 診断結果パータン(result: 結果、status: あなたの状態、advice: アドバイス、allUrl: まとめて購入リンク先)
+	var resultPattern = {
+		a:   { result: 'result_02.png', status: 'status_01.png', advice: 'adv_01.png', 
+				allUrl: 'http://item.rakuten.co.jp/cerapure/ks-c/#cart' },
+		aa:  { result: 'result_04.png', status: 'status_03.png', advice: 'adv_03.png', 
+				allUrl: 'http://item.rakuten.co.jp/cerapure/ks-d/#cart'},
+		b:   { result: 'result_03.png', status: 'status_02.png', advice: 'adv_02.png',
+				allUrl: 'http://item.rakuten.co.jp/cerapure/ktk-a/#cart' },
+		bb:  { result: 'result_05.png', status: 'status_02.png', advice: 'adv_04.png',
+				allUrl: 'http://item.rakuten.co.jp/cerapure/ktk-b/#cart' },
+		sb:  { result: 'result_05.png', status: 'status_04.png', advice: 'adv_04.png',
+				allUrl: 'http://item.rakuten.co.jp/cerapure/km-c/#cart' },
+		sbb: { result: 'result_07.png', status: 'status_04.png', advice: 'adv_08.png',
+				allUrl: 'http://item.rakuten.co.jp/cerapure/km-d/#cart' },
+		c:   { result: 'result_08.png', status: 'status_06.png', advice: 'adv_07.png',
+				allUrl: 'http://item.rakuten.co.jp/cerapure/ke-a/#cart' },
+		cc:  { result: 'result_09.png', status: 'status_06.png', advice: 'adv_09.png',
+				allUrl: 'http://item.rakuten.co.jp/cerapure/ke-c/#cart' },
+		wa:  { result: 'result_01.png', status: 'status_05.png', advice: 'adv_05.png',
+				allUrl: 'http://item.rakuten.co.jp/cerapure/kis-e/#cart' },
+		wb:  { result: 'result_06.png', status: 'status_05.png', advice: 'adv_05.png',
+				allUrl: 'http://item.rakuten.co.jp/cerapure/ktk-e/#cart' },
+		swb: { result: 'result_10.png', status: 'status_07.png', advice: 'adv_10.png',
+				allUrl: 'http://item.rakuten.co.jp/cerapure/kio-e/#cart' },
+		wc:  { result: 'result_11.png', status: 'status_08.png', advice: 'adv_11.png',
+				allUrl: 'http://item.rakuten.co.jp/cerapure/ka-st/#cart' }
+	};
+
+	// 結果判定ルール(result: 結果パターン、goods:　商品表示パターン)
+	var rules = {
+		"AAA-A-A-AA": { num: '01', result: resultPattern.a,   goods: goodsPattern.a   },
+		"AAA-A-A-BA": { num: '02', result: resultPattern.a,   goods: goodsPattern.a   },
+		"AAA-A-B--A": { num: '03', result: resultPattern.b,   goods: goodsPattern.b   },
+		"AAB-A-A-AA": { num: '04', result: resultPattern.aa,  goods: goodsPattern.aa  },
+		"AAB-A-A-BA": { num: '05', result: resultPattern.sb,  goods: goodsPattern.aa  },
+		"AAB-A-B--A": { num: '06', result: resultPattern.aa,  goods: goodsPattern.sb  },
+		"AAB-B-A-AA": { num: '07', result: resultPattern.aa,  goods: goodsPattern.aa  },
+		"AAB-B-A-BA": { num: '08', result: resultPattern.aa,  goods: goodsPattern.aa  },
+		"AAB-B-B--A": { num: '09', result: resultPattern.bb,  goods: goodsPattern.bb  },
+		"ABA---A-AA": { num: '10', result: resultPattern.a,   goods: goodsPattern.a   },
+		"ABA---A-BA": { num: '11', result: resultPattern.a,   goods: goodsPattern.a   },
+		"ABA---B--A": { num: '12', result: resultPattern.b,   goods: goodsPattern.b   },
+		"ABB---A-AA": { num: '13', result: resultPattern.a,   goods: goodsPattern.a   },
+		"ABB---A-BA": { num: '14', result: resultPattern.a,   goods: goodsPattern.a   },
+		"ABB---B--A": { num: '15', result: resultPattern.b,   goods: goodsPattern.b   },
+		"BAA---A--A": { num: '16', result: resultPattern.wa,  goods: goodsPattern.wa  },
+		"BAA---B--A": { num: '17', result: resultPattern.wb,  goods: goodsPattern.wb  },
+		"BAB---A--A": { num: '18', result: resultPattern.wa,  goods: goodsPattern.wa  },
+		"BAB---B--A": { num: '19', result: resultPattern.wb,  goods: goodsPattern.wb  },
+		"BBA---A--A": { num: '20', result: resultPattern.wa,  goods: goodsPattern.wa  },
+		"BBA---B--A": { num: '21', result: resultPattern.wb,  goods: goodsPattern.wb  },
+		"BBB---A--A": { num: '22', result: resultPattern.wa,  goods: goodsPattern.wa  },
+		"BBB---B--A": { num: '23', result: resultPattern.wb,  goods: goodsPattern.wb  },
+		"AAA-A-A-AB": { num: '24', result: resultPattern.sb,  goods: goodsPattern.sb  },
+		"AAA-A-A-BB": { num: '25', result: resultPattern.sb,  goods: goodsPattern.sb  },
+		"AAA-A-B--B": { num: '26', result: resultPattern.c,   goods: goodsPattern.c   },
+		"AAB-A-A-AB": { num: '27', result: resultPattern.sbb, goods: goodsPattern.sbb },
+		"AAB-A-A-BB": { num: '28', result: resultPattern.sbb, goods: goodsPattern.sbb },
+		"AAB-A-B--B": { num: '29', result: resultPattern.cc,  goods: goodsPattern.cc  },
+		"AAB-B-A-AB": { num: '30', result: resultPattern.sbb, goods: goodsPattern.sbb },
+		"AAB-B-A-BB": { num: '31', result: resultPattern.sbb, goods: goodsPattern.sbb },
+		"AAB-B-B--B": { num: '32', result: resultPattern.cc,  goods: goodsPattern.cc  },
+		"ABA---A-AB": { num: '33', result: resultPattern.sb,  goods: goodsPattern.sb  },
+		"ABA---A-BB": { num: '34', result: resultPattern.sb,  goods: goodsPattern.sb  },
+		"ABA---B--B": { num: '35', result: resultPattern.c,   goods: goodsPattern.c   },
+		"ABB---A-AB": { num: '36', result: resultPattern.sb,  goods: goodsPattern.sb  },
+		"ABB---A-BB": { num: '37', result: resultPattern.sb,  goods: goodsPattern.sb  },
+		"ABB---B--B": { num: '38', result: resultPattern.c,   goods: goodsPattern.c   },
+		"BAA---A--B": { num: '39', result: resultPattern.swb, goods: goodsPattern.swb },
+		"BAA---B--B": { num: '40', result: resultPattern.wc,  goods: goodsPattern.wc  },
+		"BAB---A--B": { num: '41', result: resultPattern.swb, goods: goodsPattern.swb },
+		"BAB---B--B": { num: '42', result: resultPattern.wc,  goods: goodsPattern.wc  },
+		"BBA---A--B": { num: '43', result: resultPattern.swb, goods: goodsPattern.swb },
+		"BBA---B--B": { num: '44', result: resultPattern.wc,  goods: goodsPattern.wc  },
+		"BBB---A--B": { num: '45', result: resultPattern.swb, goods: goodsPattern.swb },
+		"BBB---B--B": { num: '46', result: resultPattern.wc,  goods: goodsPattern.wc  }
+	};
+
+	$("#question-section").empty();
+	$("#question-template").tmpl({ list: questionList }).appendTo("#question-section");
 
 	// 選択結果
 	var answerList = '';
@@ -116,6 +144,8 @@ window.onload = function() {
 			var d = $(this).parent().parent().attr("id");
             $("#" + d).hide();
             $("#start").show();
+            // 画面トップの戻る
+			$("body").scrollTop(0);
 		} else if(value == "A" || value == "B") {
 			// 選択結果保存
 			answerList += value;
@@ -150,27 +180,7 @@ window.onload = function() {
 		}
 		console.log("result=" + result);
 
-		// goodsList表示内容作成
-		var getGoodsList = function(goodsList) {
-			console.log("goodsList=" + goodsList);
-			var content = '';
-			$.each(goodsList.split(','), function(index, imageName){
-				var stempNum = parseInt(index + 1);
-				content += "<div class='goods' "
-					+ " style='background: url(images/" 
-					+ imageName + ") center top no-repeat;'>" 
-					+ "step" + stempNum+ "</div>";
-			});
-			return content;
-		};
-
-		$("#result").loadTemplate(
-			$("#result-template"), {
-				result: "images/" + result.result,
-				status: "images/" + result.status,
-				advice: "images/" + result.advice,
-				goodsList: getGoodsList(result.goods)
-			}
-		);
+		$("#result").empty();
+		$("#result-template").tmpl(result).appendTo("#result");
 	};
 }
